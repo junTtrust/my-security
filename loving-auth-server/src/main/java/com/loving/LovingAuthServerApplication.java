05799f7bbf5e4973f100f7b628151655fe681090
yijunjun <25388011977@qq.com>
2021-02-27 13:58:23 +0800

    Initial commit

---
+++ loving-auth-server/src/main/java/com/loving/LovingAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.loving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovingAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovingAuthServerApplication.class, args);
+    }
+
+}
---
+++ loving-auth-server/src/main/java/com/loving/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.loving.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 授权中心安全配置
+ * @Author: yijunjun
+ * @Date: 2021/2/24 21:56
+ */
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+
+{
+}
