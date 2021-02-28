bc29cc276184fa0d2c1fbccb4a344ac1f632b01d
yijunjun <25388011977@qq.com>
2021-02-28 15:42:58 +0800

    2021/02/28 资源服务器访问接入oauth

---
+++ loving-order/src/main/java/com/loving/LovingOrderApplication.java
@@ -0,0 +1,13 @@
+package com.loving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovingOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovingOrderApplication.class, args);
+    }
+
+}
